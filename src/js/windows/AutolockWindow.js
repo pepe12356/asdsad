34e616c43f1f990b7abd373134ea01ee1f99b010
MrPepe1 <kulipeter74@gmail.com>
2018-10-13 07:10:09 +0200

    Add files via upload

--- src/js/windows/AutolockWindow.js
+++ src/js/windows/AutolockWindow.js
@@ -3,58 +3,31 @@ Created by Freshek on 28.10.2017
 */
 
 class AutolockWindow {
-    createWindow() {
-      this.autolockWindow = WindowFactory.createWindow({
-        width: 320,
-        text: "Autolocker"
-      });
-  
-      let options = [
-        {
-          name: 'lockPlayers',
-          labelText: 'Autolock Players (key: z)',
-          appendTo: this.autolockWindow,
-          event: function () {
-            window.settings.settings.lockPlayers = this.checked;
-          }
-        },
-        {
-          name: 'autoAttack',
-          labelText: 'Auto attack Player after lock',
-          appendTo: this.autolockWindow,
-          event: function () {
-            window.settings.settings.autoAttack = this.checked;
-          }
-        },
-        {
-          name: 'lockNpc',
-          labelText: 'Autolock NPCs (key: x)',
-          appendTo: this.autolockWindow,
-          event: function () {
-            window.settings.settings.lockNpc = this.checked;
-          }
-        },
-        {
-          name: 'excludeNpcs',
-          labelText: 'Exclude NPCs from locking',
-          appendTo: this.autolockWindow,
-          event: function () {
-            window.settings.settings.excludeNpcs = this.checked;
-          }
-        },
-        {
-          name: 'autoAttackNpcs',
-          labelText: 'Auto attack NPC after lock',
-          appendTo: this.autolockWindow,
-          event: function () {
-            window.settings.settings.autoAttackNpcs = this.checked;
-          }
-        },
-      ];
-  
-      options.forEach((option) => {
-        this[option.name] = ControlFactory.createControl(option);
-      });
-  
-    }
+  createWindow() {
+    this.autolockWindow = WindowFactory.createWindow({width: 300, text: "jelölő"});
+
+    let options = [
+      {
+        name: 'lockNpc',
+        labelText: 'Npc jelölés (gomb: x)',
+        appendTo: this.autolockWindow,
+        event: function () {
+          window.settings.lockNpc = this.checked;
+        }
+      },
+      {
+        name: 'lockPlayers',
+        labelText: 'Player jelölés (gomb: z)',
+        appendTo: this.autolockWindow,
+        event: function () {
+          window.settings.lockPlayers = this.checked;
+        }
+      },
+    ];
+
+    options.forEach((option)=>{
+      this[option.name] = ControlFactory.checkbox(option);
+    });
+
   }
+}
